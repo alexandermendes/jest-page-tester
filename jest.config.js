5b77d1739fc1a0cde07671eb83678f867f7ce0ed
alexmendes <alexander.mendes@immediate.co.uk>
2021-07-23 21:49:29 +0100

    build: remove useless collectCoverageFrom setting

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,5 @@
 module.exports = {
   clearMocks: true,
-  collectCoverageFrom: ['src/{**/,}*.js'],
   preset: './jest-preset',
   transform: { '^.+\\.(js|jsx)?$': '<rootDir>/node_modules/babel-jest' },
 };
