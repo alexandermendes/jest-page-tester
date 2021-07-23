51eed94311b93fb9d3256903f9509196ebb99529
alexmendes <alexander.mendes@immediate.co.uk>
2021-07-23 22:14:35 +0100

    feat: add raf-polyfill

--- jest-preset.js
+++ jest-preset.js
@@ -3,7 +3,7 @@ const path = require('path');
 module.exports = {
   clearMocks: true,
   maxWorkers: 1,
-  setupFilesAfterEnv: [path.join(__dirname, 'dist', 'setup.js')],
+  setupFilesAfterEnv: [path.join(__dirname, 'dist', 'polyfills.js')],
   testEnvironment: path.join(__dirname, 'dist', 'index.js'),
   testTimeout: 60000,
   verbose: true,
--- src/polyfills.js
+++ src/polyfills.js
@@ -1,5 +1,5 @@
+import 'raf-polyfill';
 import MatchMediaMock from 'jest-matchmedia-mock';
 
-// Polyfill matchMedia
 // eslint-disable-next-line no-new
 new MatchMediaMock();
