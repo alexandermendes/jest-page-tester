0fa7225fc4b1e5cbdb1546928b57280b5a1218a4
alexmendes <alexander.mendes@immediate.co.uk>
2021-08-05 00:15:04 +0100

    fix: patch with new semantic release config

---
+++ release.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/npm',
+    '@semantic-release/changelog',
+    '@semantic-release/release-notes-generator',
+    '@semantic-release/github',
+  ],
+};
